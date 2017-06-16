659518afc75390c63bd6d68f12b2abac4979e63f
Martin Wragg <mdwragg@gmail.com>
2017-06-16 16:57:32 -0700

    First commit

---
+++ dist/px-app-helpers.js
@@ -0,0 +1,2 @@
+'use strict';(function(){Polymer({is:'px-app-helpers',properties:{counterValue:{type:Number,value:0,notify:true}},handleClick:function handleClick(event,detail,sender){this.increment()},increment:function increment(){this.counterValue++}})})();
+//# sourceMappingURL=px-app-helpers.js.map
