c3f3f31e452dd63e75721bac913841a1fc617633
David Leonard <davidrobertleonard@gmail.com>
2017-07-28 15:09:40 -0700

    Remove ES6 features so code is valid ES5 syntax

--- px-app-route/px-app-route-helpers.js
+++ px-app-route/px-app-route-helpers.js
@@ -4,7 +4,7 @@
       if (!Array.isArray(a1) || !Array.isArray(a2)) return false;
       if (a1.length !== a2.length) return false;
 
-      for (let i=0; i<a1.length; i++) {
+      for (var i=0; i<a1.length; i++) {
         if (a1[i] !== a2[i]) {
           return false;
         }
